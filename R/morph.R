480d1bd1049ff5ab16303c19c21aa77bcd1d3a78
Amy <zhang.amyx@gmail.com>
2023-01-08 09:16:39 -0800

    Added split_examples()
    Puts line break before examples to separate from definitions

--- R/morph.R
+++ R/morph.R
@@ -2,16 +2,19 @@
 #' @importFrom dplyr group_by n
 #' @importFrom rlang ensym !!
 merge_dupes <- function(df, word_col = "word", reading_col = "pinyin",
-                        defn_col = "definition") {
+                        defn_col = "definition", html = TRUE) {
   word <- ensym(word_col)
   pyin <- ensym(reading_col)
   defn <- ensym(defn_col)
 
+  separator <- ifelse(html, "\n\n<hr style='width: 30%'><hr style='width: 30%'>\n\n",
+                      "\n\n===\n\n")
   df |>
     dplyr::group_by(!!word) |>
     dplyr::summarise(
       !!defn := ifelse(n() > 1,
-                       paste(paste(!!pyin, !!defn, sep = "\n"), collapse = "\n===\n"),
+                       paste(paste(!!pyin, !!defn, sep = "\n"),
+                             collapse = separator),
                        !!defn),
       !!pyin := paste(!!pyin, collapse = "; ")
     ) |>
@@ -73,14 +76,28 @@ split_simplified <- function(df, word_col = "word", brackets = "traditional") {
 #' @describeIn anno_both Remove matching entries from specified purge column.
 #' @export
 purge_same <- function(df, cols = c("simplified", "traditional"), purge = "simplified") {
-  dupes_col <- df[[cols[purge]]]
-  df[[cols[purge]]] <- ifelse(dupes_col == df[[cols[-purge]]], "", dupes_col)
+  dupes_col <- df[[purge]]
+  other_col <- df[[setdiff(cols, purge)]]
+  df[[purge]] <- ifelse(dupes_col == other_col, "", dupes_col)
   df
 }
 
-separate_lists <- function(df, format = "\\d", defn_col = "definition") {
+split_examples <- function(df, defn_col = "definition") {
+  # Examples are preceded by the text in chinese, then by pinyin, then by
+  # an English translation. Multiple examples are listed with a single
+  # lowercase letter of the Latin alphabet, e.g. "a (example 1) b (example 2)".
+  df[[defn_col]] <- gsub(" ([a-z] )*(\\p{Han}+) (\\w)", "\n\t\\1\\2 \\3",
+                         df[[defn_col]], perl = TRUE)
+  df
+}
+
+#' @param double_break Insert a double line break before items in numbered lists.
+#'   Useful when there are examples that have a lot of text.
+separate_lists <- function(df, format = "\\d", defn_col = "definition",
+                           double_break = FALSE) {
+  repl <- ifelse(double_break, "\n\n\\1", "\n\\1")
   defn <- df[[defn_col]] |>
-    str_replace_all(paste0(" (", format, ")"), "\n\\1")
+    str_replace_all(paste0(" (", format, ")"), repl)
   df[[defn_col]] <- defn
   df
 }
